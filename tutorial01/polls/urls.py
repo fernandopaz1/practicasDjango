562b51ad2487bfe282ff081ec7008dd0acd79307
fernandopaz1 <ferpaz9292@gmail.com>
2021-06-10 04:58:22 -0300

    usando templates e implementando error 404

--- tutorial01/polls/urls.py
+++ tutorial01/polls/urls.py
@@ -7,4 +7,13 @@ urlpatterns = [
     # el primer campo es el la direccion mediante la cual accedo a la funcion en el navegador
     # esta dirección esta montada sobre el directrio polls/
     path('about/', views.about, name='about'),  # Incluyo la respuesta de views.about en localhost/polls/about
+
+    path('<int:question_id>/', views.detail, name='detail'),  # añadimos una vista con un path custom segun el question_id
+    path('<int:question_id>/results/', views.results, name='results'),  # añadimos una vista con un path custom segun el id que vemos
+
+    #'<int:question_id>/' es lo que queda luego de que urlpatterns le quete la parte de polls ==> identifica el numero que le pasemos en la url
+    # como '<int:question_id>/' = '34/' entonces a las vistas le pasa el valor 34
+
+    path('<int:question_id>/vote/', views.vote, name='vote'),  # añadimos una vista con un path custom segun el id que vemos
 ]
+
