3aae2d03d683a2b689887b24cb36a9b18c13a6b5
fernandopaz1 <ferpaz9292@gmail.com>
2021-06-11 18:13:39 -0300

    pasando vistas a vistas genericas

--- tutorial01/polls/urls.py
+++ tutorial01/polls/urls.py
@@ -5,13 +5,13 @@ from . import views
 
 app_name = 'polls'  # para que django no se confunda si tengo 20 apps con un detail cada una 
 urlpatterns = [
-    path('', views.index, name='index'),  # incluyo la respuesta de views.index en localhost/polls/  <-- despues de polls hay un string vacio
+    path('', views.IndexView.as_view(), name='index'),  # incluyo la respuesta de views.index en localhost/polls/  <-- despues de polls hay un string vacio
     # el primer campo es el la direccion mediante la cual accedo a la funcion en el navegador
     # esta dirección esta montada sobre el directrio polls/
     path('about/', views.about, name='about'),  # Incluyo la respuesta de views.about en localhost/polls/about
 
-    path('detalle/<int:question_id>/', views.detail, name='detail'),  # añadimos una vista con un path custom segun el question_id
-    path('<int:question_id>/results/', views.results, name='results'),  # añadimos una vista con un path custom segun el id que vemos
+    path('detalle/<int:pk>/', views.DetailView.as_view(), name='detail'),  # añadimos una vista con un path custom segun el question_id
+    path('<int:pk>/results/',views.ResultsView.as_view(), name='results'),  # añadimos una vista con un path custom segun el id que vemos
 
     #'<int:question_id>/' es lo que queda luego de que urlpatterns le quete la parte de polls ==> identifica el numero que le pasemos en la url
     # como '<int:question_id>/' = '34/' entonces a las vistas le pasa el valor 34
--- tutorial01/polls/views.py
+++ tutorial01/polls/views.py
@@ -1,32 +1,33 @@
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
 # Create your views here.
 
 from .models import Question
 from .models import Choice
 
-def index(request):  # reques es un objeto tipo HttpRequest ?           
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list
-    }
-    return render(request, 'polls/index.html', context)  # renderea el template con el contexto que le paso
+# def index(request):  # reques es un objeto tipo HttpRequest ?           
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     # template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list
+#     }
+#     return render(request, 'polls/index.html', context)  # renderea el template con el contexto que le paso
 
 def about(request):
     return HttpResponse("Soy una página de pruebas!!!")
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("La pregunta no existe")
-    return render(request, 'polls/detail.html', {'question': question})
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("La pregunta no existe")
+#     return render(request, 'polls/detail.html', {'question': question})
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
@@ -41,4 +42,25 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-    return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    
+
+# vistas genericas: 
+class IndexView(generic.ListView):               
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
