d872192227d7980fb9fbe7437f957a89b18717da
fernandopaz1 <ferpaz9292@gmail.com>
2021-06-11 17:55:38 -0300

    get_object_or_404 en todas las vistas

--- tutorial01/polls/urls.py
+++ tutorial01/polls/urls.py
@@ -2,13 +2,15 @@ from django.urls import path
 
 from . import views
 
+
+app_name = 'polls'  # para que django no se confunda si tengo 20 apps con un detail cada una 
 urlpatterns = [
     path('', views.index, name='index'),  # incluyo la respuesta de views.index en localhost/polls/  <-- despues de polls hay un string vacio
     # el primer campo es el la direccion mediante la cual accedo a la funcion en el navegador
     # esta dirección esta montada sobre el directrio polls/
     path('about/', views.about, name='about'),  # Incluyo la respuesta de views.about en localhost/polls/about
 
-    path('<int:question_id>/', views.detail, name='detail'),  # añadimos una vista con un path custom segun el question_id
+    path('detalle/<int:question_id>/', views.detail, name='detail'),  # añadimos una vista con un path custom segun el question_id
     path('<int:question_id>/results/', views.results, name='results'),  # añadimos una vista con un path custom segun el id que vemos
 
     #'<int:question_id>/' es lo que queda luego de que urlpatterns le quete la parte de polls ==> identifica el numero que le pasemos en la url
--- tutorial01/polls/views.py
+++ tutorial01/polls/views.py
@@ -1,9 +1,10 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-
+from django.urls import reverse
 # Create your views here.
 
 from .models import Question
+from .models import Choice
 
 def index(request):  # reques es un objeto tipo HttpRequest ?           
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -29,4 +30,15 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("Estas votando la pregunta %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': 'No seleccionaste una opción',
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
